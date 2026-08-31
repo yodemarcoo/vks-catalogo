440fde805c23f8478b7fde88506c9f17e89b9fbd
yodemarcoo <yohanna.marco@pucpr.edu.br>
2026-08-31 13:41:14 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/vks-catalogo/',
   plugins: [solidPlugin()],
   build: {
     target: 'es2022',
